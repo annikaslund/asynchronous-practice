c250d5fa22b64beed84e355f1b2563793f93e90a
annika <annikaslund@gmail.com>
2019-03-04 20:40:43 -0800

    Add card drawing functionality, appends to DOM

---
+++ part_2/cards.js
@@ -0,0 +1,29 @@
+// calls deck of cards API and returns a response of a shuffled deck.
+async function shuffle_cards(){
+    let response = await $.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+
+    return response;
+}
+
+// draws a card from the deck.
+async function draw_card(){
+    let deck = await shuffle_cards();
+    let deckID = deck.deck_id;
+    let response = await $.get(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+
+    return response;
+}
+
+async function generate_card_HTML(){
+    let card = await draw_card();
+    let HTMLcard = `<img src="${card.cards[0].image}">`;
+
+    $("#cards-container").append(HTMLcard);
+}
+
+$("#draw-card").on("submit", async function(e){
+    e.preventDefault()
+    $("#cards-container").empty();
+
+    generate_card_HTML();
+})
